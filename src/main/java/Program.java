658ee730d4c1283faa87e1d5aa05a623aaa32410
Anton Iankin <kerjen01@gmail.com>
2022-06-17 00:15:26 +0300

    [FEATURE] Changed jobs runner

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,43 +1,20 @@
+import job.PhoneCounterJob;
 import map.OrdersCountMapper;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.ToolRunner;
 import reduce.OrdersCountReducer;
 
 import java.io.IOException;
 
 public class Program {
     public static void main(String[] args) {
-        JobConf job_conf = new JobConf(Program.class);
-        job_conf.setJobName("Orders");
-
-        job_conf.setOutputKeyClass(Text.class);
-        job_conf.setOutputValueClass(IntWritable.class);
-
-        job_conf.setMapperClass(OrdersCountMapper.class);
-        job_conf.setReducerClass(OrdersCountReducer.class);
-
-        job_conf.setInputFormat(TextInputFormat.class);
-        job_conf.setOutputFormat(TextOutputFormat.class);
-
-
-        FileInputFormat.setInputPaths(job_conf, new Path("input"));
-        FileOutputFormat.setOutputPath(job_conf, new Path("output"));
-
         try {
-            runJob(job_conf);
+            int exitCode = ToolRunner.run(new PhoneCounterJob(), args);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static RunningJob runJob(JobConf job) throws Exception {
-        JobClient jc = new JobClient(job);
-        RunningJob rj = jc.submitJob(job);
-        if (!jc.monitorAndPrintJob(job, rj)) {
-            throw new IOException("Job failed with info: " + rj.getFailureInfo());
-        }
-        return rj;
     }
 }
---
+++ src/main/java/job/PhoneCounterJob.java
@@ -0,0 +1,37 @@
+package job;
+
+import map.OrdersCountMapper;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import reduce.OrdersCountReducer;
+
+public class PhoneCounterJob extends Configured implements Tool {
+    @Override
+    public int run(String[] strings) throws Exception {
+        JobConf job_conf = new JobConf(getClass());
+        job_conf.setJobName("Orders");
+
+        job_conf.setOutputKeyClass(Text.class);
+        job_conf.setOutputValueClass(IntWritable.class);
+
+        job_conf.setMapperClass(OrdersCountMapper.class);
+        job_conf.setReducerClass(OrdersCountReducer.class);
+
+        job_conf.setInputFormat(TextInputFormat.class);
+        job_conf.setOutputFormat(TextOutputFormat.class);
+
+
+        FileInputFormat.setInputPaths(job_conf, new Path("input"));
+        FileOutputFormat.setOutputPath(job_conf, new Path("output"));
+
+
+        Job job = Job.getInstance(job_conf, "PhoneCounter");
+
+        return job.waitForCompletion(true) ? 0 :1;
+    }
+}
